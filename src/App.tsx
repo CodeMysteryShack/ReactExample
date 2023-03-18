6e3d1145d9558fb971b831815a8bc776c2e90df4
Pyotr Ryzhov <maestro_pete@inbox.ru>
2023-03-18 21:57:10 +0300

    fixed gh-pages homepage4

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       <p className="read-the-docs">
-        ©2023 v.2.0.0
+        ©2023
       </p>
     </div>
   )
